f2619f5bf5c61d6536d7e78050d223c5ee560101
Roman Turner <rn.turner@live.com>
2021-04-26 17:00:16 -0700

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ActiveRecord::Base
 
+        @@all = []
+        def initialize(title, description)
+            @title, @description = title, description 
+            @all << self
+        end 
+
+
+
 end
